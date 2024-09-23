41c5165d54c00d38f968c4c66ea85b40e0d7f8de
jumengbo <2506741626@qq.com>
2024-09-23 03:54:53 -0400

    add substation.yaml

--- train.py
+++ train.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLOv10
 
 
-model = YOLOv10('./ultralytics/cfg/models/v10/yolov10_ODC.yaml')
+model = YOLOv10('./ultralytics/cfg/models/v10/yolov10s.yaml')
 
-model.train(data='screen.yaml', epochs=500, batch=32, imgsz=640)
+model.train(data='substation.yaml', epochs=200, batch=32, imgsz=640)
 
 #yolo detect train data=screen.yaml model=yolov10s.yaml epochs=10 batch=8 imgsz=640 device=0
